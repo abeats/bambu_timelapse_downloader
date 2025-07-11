7ce6c4bd6f908cb05becf64e278b451a43ee7393
foray-austin <a.bernstein.127@gmail.com>
2025-07-10 19:54:57 -0700

    Console output changes
    - Simplify outputs
    - Add '<current-file-#/files-in-batch>' to output

--- bambu_timelapse_download.py
+++ bambu_timelapse_download.py
@@ -9,7 +9,7 @@ from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
 app_name = __name__
-version = '1.0.0.0'
+version = '1.0.0.1'
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -17,40 +17,38 @@ elif __file__:
     application_path = os.path.dirname(__file__)
 
 
-def setup_logging(log_root_directory=f'{application_path}/logs', logger_name=app_name, log_file_max_byte_size=104857, log_file_max_backup=3, log_default_level=logging.DEBUG, console_level=logging.INFO, logfile_level=logging.DEBUG):
+def setup_logging(log_root_directory=f'{application_path}/logs', logger_name=app_name,
+                  log_file_max_byte_size=104857, log_file_max_backup=3,
+                  log_default_level=logging.DEBUG, console_level=logging.INFO, logfile_level=logging.DEBUG):
     """
-    :param logger_name:
-    :param default_level:
-    :return: logger
+    Sets up logging to console and rotating log file.
     """
     today = datetime.today()
     year = today.strftime('%Y')
     month = today.strftime('%m')
     log_name_ = logger_name.rstrip('.log') + f'_{os.getlogin()}' + '.log'
     log_name = today.strftime(f'%Y%m%d_{log_name_}')
-    # create an initial logger. It will only log to console and it will disabled
-    log_format_console = "[%(asctime)s:%(filename)s:%(lineno)s:%(name)s.%(funcName)s()] %(levelname)s %(message)s"
+
+    log_format_console = "[%(asctime)s] %(levelname)s %(message)s"  # <- Simplified console format
     log_format_file = "[%(asctime)s:%(filename)s:%(lineno)s:%(name)s.%(funcName)s()] %(levelname)s %(message)s"
-    log_date_format = '%Y%m%d %H:%M:%S'
-    if logger_name:
-        logger = logging.getLogger(logger_name)
-    else:
-        logger = logging.getLogger()
+    log_date_format = '%Y-%m-%d %H:%M:%S'
+
+    logger = logging.getLogger(logger_name) if logger_name else logging.getLogger()
     log_directory = f'{log_root_directory}/{year}/{month}'
+
     if not os.path.exists(log_directory):
-        logger.debug(f'create log dir {log_directory}')
         os.makedirs(log_directory)
 
     log_file_path = f'{log_directory}/{log_name}'
-    logger.debug(f'add log file handler {log_file_path}')
     log_file_handler = RotatingFileHandler(filename=log_file_path, mode='a',
                                            maxBytes=log_file_max_byte_size,
                                            backupCount=log_file_max_backup,
-                                           delay="false",
+                                           delay=False,
                                            encoding='utf8')
+
     log_console_handler = logging.StreamHandler()
     log_console_handler.setLevel(console_level)
-    log_console_handler.setFormatter(logging.Formatter(log_format_console))
+    log_console_handler.setFormatter(logging.Formatter(log_format_console, datefmt=log_date_format))
 
     log_file_handler.setLevel(logfile_level)
     log_file_handler.setFormatter(logging.Formatter(log_format_file, datefmt=log_date_format))
@@ -59,6 +57,7 @@ def setup_logging(log_root_directory=f'{application_path}/logs', logger_name=app
     logger.addHandler(log_console_handler)
     logger.addHandler(log_file_handler)
     return logger
+
 
 class ImplicitFTP_TLS(ftplib.FTP_TLS):
     """FTP_TLS subclass that automatically wraps sockets in SSL to support implicit FTPS."""
@@ -69,12 +68,10 @@ class ImplicitFTP_TLS(ftplib.FTP_TLS):
 
     @property
     def sock(self):
-        """Return the socket."""
         return self._sock
 
     @sock.setter
     def sock(self, value):
-        """When modifying the socket, ensure that it is ssl wrapped."""
         if value is not None and not isinstance(value, ssl.SSLSocket):
             value = self.context.wrap_socket(value)
         self._sock = value
@@ -101,52 +98,57 @@ def ftp_download(args):
         if args.ftp_timelapse_folder in ftp_client.nlst():
             ftp_client.cwd(args.ftp_timelapse_folder)
             try:
-                logger.info('Looking avi files for download.')
+                logger.info('Looking for .avi files to download...')
                 ftp_timelapse_files = [f for f in ftp_client.nlst() if f.endswith('.avi')]
                 ftp_timelapse_files = [f for f in ftp_timelapse_files if f not in downloaded_files]
 
-                if ftp_timelapse_files:
-                    logger.info(f'Found {len(ftp_timelapse_files)} files for download.')
-                    for f in ftp_timelapse_files:
+                total_files = len(ftp_timelapse_files)
+
+                if total_files:
+                    logger.info(f'Found {total_files} files for download.')
+                    for index, f in enumerate(ftp_timelapse_files, start=1):
                         filesize = ftp_client.size(f)
-                        filesize_mb = round(filesize/1024/1024, 2)
-                        download_file_name = f
-                        download_file_path = f'{args.download_dir}/{download_file_name}'
+                        filesize_mb = round(filesize / 1024 / 1024, 2)
+                        download_file_path = f'{args.download_dir}/{f}'
+
                         if filesize == 0:
-                            logger.info(f'Filesize of file {f} is 0, skipping file and continue')
+                            logger.info(f'Filesize of file {f} is 0, skipping file and continuing...')
                             continue
                         try:
-                            logger.info(f'Downloading file "{f}" size: {filesize_mb} MB')
-                            fhandle = open(download_file_path, 'wb')
-                            ftp_client.retrbinary('RETR %s' % f, fhandle.write)
+                            logger.info(f'Downloading file [{index}/{total_files}] "{f}" size: {filesize_mb} MB')
+                            with open(download_file_path, 'wb') as fhandle:
+                                ftp_client.retrbinary('RETR %s' % f, fhandle.write)
                             if args.delete_files_from_sd_card_after_download:
                                 try:
                                     ftp_client.delete(f)
                                 except Exception as e:
-                                    logger.error('Failed to delete file after download, continue with next file')
+                                    logger.error(f'Failed to delete file after download: {e}, continuing...')
                                     continue
                         except Exception as e:
-                            fhandle.close()
-                            os.remove(download_file_path)
-                            logger.error(f'failed to download file {f}: {e}, continue with next file')
+                            if os.path.exists(download_file_path):
+                                os.remove(download_file_path)
+                            logger.error(f'Failed to download file {f}: {e}, continuing...')
                             continue
+                else:
+                    logger.info("No new .avi files to download.")
             except ftplib.error_perm as resp:
                 if str(resp) == "550 No files found":
                     logger.error("No files in this directory")
                 else:
                     raise
         else:
-            logger.info(f'{args.ftp_timelapse_folder} not found on ftp server.')
+            logger.info(f'{args.ftp_timelapse_folder} not found on FTP server.')
             sys.exit(1)
     except Exception as e:
-        logger.error(f'Programm failed: {e}')
+        logger.error(f'Program failed: {e}')
         sys.exit(1)
+
 
 if __name__ == '__main__':
     logger = setup_logging()
 
-    logger.info(f'Starting bambu timelapse downloader v{version}')
-    parser = argparse.ArgumentParser(description='Download bambu timelaps from printer ftp server.')
+    logger.info(f'Starting Bambu timelapse downloader v{version}')
+    parser = argparse.ArgumentParser(description='Download Bambu timelapses from printer FTP server.')
     parser.add_argument('--ip', type=str)
     parser.add_argument('--port', type=int, default=990, required=False)
     parser.add_argument('--user', type=str, default='bblp', required=False)
